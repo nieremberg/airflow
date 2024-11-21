147b1921f2a874f15a4fe32e94200c903c81226f
nieremberg <nieremberg@gmail.com>
2024-11-21 18:20:27 -0300

    Dependências de DAGs

---
+++ dags/dag_principal.py
@@ -0,0 +1,31 @@
+"""
+# DAG principal
+"""
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+import pendulum
+import pprint
+
+
+def print_principal(**kwargs):
+    print("DAG principal")
+    pprint.pprint(kwargs)
+    return "principal"
+
+
+with DAG(
+    dag_id="DAG_principal",
+    schedule="0 17 * * *",
+    start_date=pendulum.datetime(2024, 11, 21, tz="America/Sao_Paulo"),
+    catchup=True,
+) as dag:
+    start = EmptyOperator(task_id="start")
+    python_principal = PythonOperator(
+        task_id="python_principal",
+        python_callable=print_principal
+    )
+    end = EmptyOperator(task_id="end")
+
+start >> python_principal >> end
